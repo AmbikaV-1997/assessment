a09273c07a48634652ed1bc3df0fb38c5912b0dc
Ambika.V <ambikavenkatesh1997@gmail.com>
2022-05-11 13:55:50 +0530

    First Test Case : addition of two numbers (inputs given in string form)

---
+++ ass/addnum.py
@@ -0,0 +1,9 @@
+import re
+
+def add(x):
+    temp = re.findall(r'\d+', x)
+    res = list(map(int, temp))
+    sum = 0
+    for i in res:
+        sum += i
+    return sum
---
+++ ass/test_cases.py
@@ -0,0 +1,3 @@
+import addnum
+def test_add():
+    assert addnum.add("3,5")==8;
